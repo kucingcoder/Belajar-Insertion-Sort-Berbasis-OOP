c3d21986c2b17d856f9718269e7910628a3cd069
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2023-07-06 17:30:10 +0700

    Lupa dicommit

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class InsertionSort {
+public class InsertionSort {
+
+    private int[] data;
+
+    public InsertionSort(){}
+
+    void setData(int[] data){
+        this.data = data;
+    }
+
+    public void InsertionSort(){
+        int size = this.data.length;
+
+        for (int step = 1; step < size; step++){
+            int key = this.data[step];
+            int j = step - 1;
+
+            while (j >= 0 && key < this.data[j]){
+                this.data[j + 1] = this.data[j];
+                --j;
+            }
+            this.data[j + 1] = key;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] ages = {12, 7, 2, 1, 0, 78, 65, 95};
+        InsertionSort urutan = new InsertionSort();
+
+        System.out.print("Acak : ");
+        for (int umur : ages) {
+            System.out.print(umur + " ");
+        }
+
+        urutan.setData(ages);
+        urutan.InsertionSort();
+
+        System.out.print("\nUrut : ");
+        for (int umur: ages) {
+            System.out.print(umur + " ");
+        }
+
+        System.out.print("\nTekan apa saja untuk keluar...");
+        Scanner exit = new Scanner(System.in);
+        exit.nextLine();
     }
 }
